2ce96eace5bfadd80cad7683fa9abac32349332b
Ezhilan2 <114604700+Ezhilan2@users.noreply.github.com>
2023-05-13 11:14:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+  mustache_nose = loadImage('https://i.postimg.cc/fy1wPFM4/moustache-png-17165.png')
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function take_snapshot()
+{
+    save('Filterimage.png');
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+  
+      noseX = results[0].pose.nose.x;
+      noseY = results[0].pose.nose.y;
+      console.log("nose x = " + noseX);
+      console.log("nose y = " + noseY);
+    }
+ }
+  
+  function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache_nose, noseX - 15, noseY, 30, 30);
+  }
+  
